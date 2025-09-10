897b65604b2757a95353549d83d7fd67af0bbe1b
Rishi Jay Manimaran <rmanimaran8@login-ice-1.pace.gatech.edu>
2025-09-10 16:18:37 -0400

    edited run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=llm_opt_uv
+#SBATCH --job-name=llm_opt
 #SBATCH -t 8:00:00              		# Runtime in D-HH:MM
 #SBATCH --mem-per-gpu 16G
 #SBATCH -n 1                          # number of CPU cores
@@ -7,7 +7,7 @@
 #SBATCH --gres=gpu:1
 #SBATCH -C "A100-40GB|A100-80GB|H100|V100-16GB|V100-32GB|RTX6000|A40|L40S"
 
-echo "launching LLM Guided Evolution with UV"
+echo "launching LLM Guided Evolution"
 hostname
 
 # Load required modules
